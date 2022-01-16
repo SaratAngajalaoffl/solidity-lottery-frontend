318f3d9a47cc7a333da15e233846c67b4fe46477
SaratAngajalaoffl <saratangajala@gmail.com>
2022-01-17 03:54:15 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+
+window.ethereum.request({ method: "eth_requestAccounts" });
+
+const web3 = new Web3(window.ethereum);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contract, setContract] = useState();
+  const [manager, setManager] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const abi = [
+      {
+        constant: true,
+        inputs: [],
+        name: "manager",
+        outputs: [{ name: "", type: "address" }],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: false,
+        inputs: [],
+        name: "pickWinner",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "getPlayers",
+        outputs: [{ name: "", type: "address[]" }],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        constant: false,
+        inputs: [],
+        name: "enter",
+        outputs: [],
+        payable: true,
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        constant: true,
+        inputs: [{ name: "", type: "uint256" }],
+        name: "players",
+        outputs: [{ name: "", type: "address" }],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "constructor",
+      },
+    ];
+
+    const address = "0xddf2f640852E5efaFc23D78A3bA53330e59BE45d";
+
+    const newContract = new web3.eth.Contract(abi, address);
+
+    setContract(newContract);
+
+    (async () => {
+      setManager(await newContract.methods.manager().call());
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return <div>Hello Sarat! Manager is {manager}</div>;
 }
 
 export default App;
